eca1f9c04e1aa95eb4744e111f719ee663e24500
Danillo Oliveira <danillo.alves.o@gmail.com>
2025-03-27 12:02:24 -0300

    fix: responsive viewport meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import { RootLayoutProps } from '@utils/types.utils';
 export const metadata: Metadata = {
   title: 'Lightning Node | Danillo Oliveira',
   description: 'Lightning node ranking web app - BIPA challange',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
